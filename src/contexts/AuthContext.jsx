84ec7758ac0235e6c6e72d4c22492527bd1baa4f
Narawit74 <s65309010039@kktech.ac.th>
2024-02-02 14:55:33 +0700

    dd

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,15 +1,43 @@
-import React, { createContext, useState } from 'react'
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext()
 
 function AuthContextProvider(props) {
-  const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-  return (
-    <AuthContextProvider value={{ user, setUser }}>
-      {props.children}
-    </AuthContextProvider>
-  )
+    useEffect(() => {
+        const run = async () => {
+            try {
+                setLoading(true)
+                let token = localStorage.getItem("token")
+                if (!token) {
+                    return
+                }
+                const rs = await axios.get("http://localhost:3000/auth/me",{
+                    headers:{
+                        Authorization : `Bearer ${token}`
+                    }
+                })
+                setUser(rs.data)
+
+            } catch (err) {
+
+            } finally {
+                setLoading(false)
+            }
+
+
+        }
+        run()
+    }, [])
+    return (
+        <AuthContext.Provider value={{ user, setUser, loading }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
 }
+
 export { AuthContextProvider }
 export default AuthContext
